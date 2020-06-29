a6725ad44a6a0d824f96952a3b839f5b0de89038
shouko <imshouko@gmail.com>
2020-06-30 01:21:52 +0900

    Fix origin detection for content.fetch

--- middleware/cors.js
+++ middleware/cors.js
@@ -1,11 +1,14 @@
 const config = require('../config');
 
 module.exports = (req, res, next) => {
-  if (
-    !req.headers.origin
-    || config.allowedOrigins.indexOf(req.headers.origin) === -1
-  ) return res.sendStatus(400);
-  res.setHeader('access-control-allow-origin', req.headers.origin);
-  res.setHeader('access-control-allow-credentials', true);
-  return next();
+  try {
+    const origin = req.headers.origin || new URL(req.headers.referer).origin;
+    if (!origin || config.allowedOrigins.indexOf(origin) === -1
+    ) throw new Error();
+    res.setHeader('access-control-allow-origin', origin);
+    res.setHeader('access-control-allow-credentials', true);
+    return next();
+  } catch (e) {
+    return res.sendStatus(400);
+  }
 };
